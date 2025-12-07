405d856f993bf786d63855bec57fd717692033c8
Yu-An05983 <chiamyuan9393@gmail.com>
2025-12-07 21:08:52 +0800

    added conveyor art

--- main.gd
+++ main.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 var save_path = "user://data.save"
 
 func _ready():
@@ -35,31 +32,3 @@ func _on_check_pressed():
 		print(JSON.stringify(save_data))
 	else:
 		print("no save data")
-=======
->>>>>>> 0e244fa
-@onready var player: CharacterBody2D = $Player
-@onready var button: Button = $GUI/HBoxContainer/Button
-
-var duct = preload("res://duct.tscn")
-var ductinst = null
-var placing_duct = false
-
-func _on_button_pressed() -> void:
-	print("Starting duct placement")
-	placing_duct = true
-
-func _input(event: InputEvent) -> void:
-	if placing_duct and event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			ductinst = duct.instantiate()
-			ductinst.position = get_global_mouse_position()
-			add_child(ductinst)
-			placing_duct = false
-
-func _process(delta: float) -> void:
-	if placing_duct:
-		pass
-<<<<<<< HEAD
-=======
->>>>>>> 4f9051a (Local changes to main files)
->>>>>>> 0e244fa
